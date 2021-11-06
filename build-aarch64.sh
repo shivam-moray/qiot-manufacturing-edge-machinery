72d68c9f244a452f77a8b14871e8b01418ffefad
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-11-06 09:22:42 +0100

    update domain-model version; add ARMv8 compilation

--- build-aarch64.sh
+++ build-aarch64.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+PV=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)-aarch64
 docker login quay.io -u ${QUAY_MANUFACTURING_USERNAME} -p ${QUAY_MANUFACTURING_PASSWORD}
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
-docker build -t quay.io/qiotmanufacturing/edge-machinery:$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)-aarch64 -f src/main/docker/Dockerfile.native.multiarch .
-docker push quay.io/qiotmanufacturing/edge-machinery:$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)-aarch64
+docker rmi quay.io/qiotmanufacturing/edge-machinery:$PV --force
+docker build -t quay.io/qiotmanufacturing/edge-machinery:$PV -f src/main/docker/Dockerfile.native.multiarch .
+docker push quay.io/qiotmanufacturing/edge-machinery:$PV
--- build.sh
+++ build.sh
@@ -5,7 +5,3 @@ mvn -B clean package -Pprod,native oc:build oc:push \
           -Dquarkus.container-image.build=true \
           -Djkube.docker.push.username=${QUAY_MANUFACTURING_USERNAME} \
           -Djkube.docker.push.password=${QUAY_MANUFACTURING_PASSWORD} 
-
-# docker run --rm --privileged multiarch/qemu-user-static:register --reset
-# docker build -t quay.io/qiotmanufacturing/edge-machinery:1.0.0-beta1-aarch64 -f src/main/docker/Dockerfile.native.multiarch .
-# docker push quay.io/qiotmanufacturing/edge-machinery:1.0.0-beta1-aarch64
