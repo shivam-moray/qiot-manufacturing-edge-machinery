57323beb064329869e3e1a8b6170625b7c32df90
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-11-12 13:09:34 +0100

    depensencies upgrade; fix client to facility-manager

--- src/main/java/io/qiot/manufacturing/edge/machinery/service/registration/FacilityManagerClient.java
+++ src/main/java/io/qiot/manufacturing/edge/machinery/service/registration/FacilityManagerClient.java
@@ -1,7 +1,7 @@
 package io.qiot.manufacturing.edge.machinery.service.registration;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -19,7 +19,7 @@ import io.qiot.manufacturing.factory.commons.domain.registration.EdgeSubscriptio
 @RegisterRestClient(configKey = "facility-manager-api")
 public interface FacilityManagerClient {
 
-    @PUT
+    @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public SubscriptionResponse subscribeMachinery(
