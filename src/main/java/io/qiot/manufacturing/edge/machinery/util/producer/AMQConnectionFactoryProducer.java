ca4b24d59627795317da66601667c542416b8cad
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-11-15 21:56:28 +0100

    Using plain artemis jms client

--- src/main/java/io/qiot/manufacturing/edge/machinery/util/producer/AMQConnectionFactoryProducer.java
+++ src/main/java/io/qiot/manufacturing/edge/machinery/util/producer/AMQConnectionFactoryProducer.java
@@ -13,10 +13,14 @@ import org.apache.activemq.artemis.core.remoting.impl.netty.TransportConstants;
 import org.apache.activemq.artemis.jms.client.ActiveMQJMSConnectionFactory;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+/**
+ * @author andreabattaglia
+ *
+ */
 @ApplicationScoped
 public class AMQConnectionFactoryProducer {
 
-    @ConfigProperty(name = "qiot.artemis.ssl", defaultValue = "false")
+    @ConfigProperty(name = "qiot.artemis.sslEnabled", defaultValue = "false")
     boolean sslEnabled;
 
     @ConfigProperty(name = "quarkus.artemis.url")
@@ -27,6 +31,36 @@ public class AMQConnectionFactoryProducer {
 
     @ConfigProperty(name = "quarkus.artemis.password")
     String password;
+
+    @ConfigProperty(name = "quarkus.artemis.host")
+    String host;
+
+    @ConfigProperty(name = "quarkus.artemis.port")
+    int port;
+
+    @ConfigProperty(name = "quarkus.artemis.verifyHost")
+    boolean verifyHost;
+
+    @ConfigProperty(name = "quarkus.artemis.trustAll")
+    boolean trustAll;
+
+    @ConfigProperty(name = "quarkus.artemis.keyStoreProvider")
+    String keyStoreProvider;
+
+    @ConfigProperty(name = "quarkus.artemis.keyStorePath")
+    String keyStorePath;
+
+    @ConfigProperty(name = "quarkus.artemis.keyStorePassword")
+    String keyStorePassword;
+
+    @ConfigProperty(name = "quarkus.artemis.trustStoreProvider")
+    String trustStoreProvider;
+
+    @ConfigProperty(name = "quarkus.artemis.trustStorePath")
+    String trustStorePath;
+
+    @ConfigProperty(name = "quarkus.artemis.trustStorePassword")
+    String trustStorePassword;
 
     private ActiveMQJMSConnectionFactory connectionFactory;
 
@@ -44,25 +78,44 @@ public class AMQConnectionFactoryProducer {
         connectionFactory.close();
     }
 
-    private void initPlainConnectionFactory() {
-
-    }
-
     private ActiveMQJMSConnectionFactory initSSLConnectionFactory() {
+
         HashMap<String, Object> map = new HashMap<>();
-        map.put(TransportConstants.PROTOCOLS_PROP_NAME, "ssl");
-        map.put(TransportConstants.HOST_PROP_NAME,
-                "broker-service-edge-0-svc-rte-factory.apps.manufacturingfacility.qiot.io");
-        map.put(TransportConstants.PORT_PROP_NAME, 443);
+
+        // ./artemis producer --destination foo --user amq --password amq
+        // --message-count 10
+        // --url="tcp://broker-service-edge-0-svc-rte-factory.apps.manufacturingfacility.qiot.io:443?sslEnabled=true;keyStorePath=./client.ks;keyStorePassword=password;trustStorePath=./client.ts;trustStorePassword=password"
+
+        // ./artemis consumer --destination foo --user amq --password amq
+        // --url="tcp://broker-service-edge-0-svc-rte-factory.apps.manufacturingfacility.qiot.io:443?sslEnabled=true;keyStorePath=./client.ks;keyStorePassword=password;trustStorePath=./client.ts;trustStorePassword=password"
+
+        // map.put(TransportConstants.PROTOCOLS_PROP_NAME, "CORE");
+        // host
+        map.put(TransportConstants.HOST_PROP_NAME, host);
+        // port
+        map.put(TransportConstants.PORT_PROP_NAME, port);
+        // sslEnabled
         map.put(TransportConstants.SSL_ENABLED_PROP_NAME, true);
-        map.put(TransportConstants.TRUSTSTORE_PATH_PROP_NAME,
-                "/var/data/qiot/machinery/data/runtime/client.ks");
-        map.put(TransportConstants.KEYSTORE_PROVIDER_PROP_NAME, "PKCS12");
-        map.put(TransportConstants.KEYSTORE_PASSWORD_PROP_NAME, "password");
-        map.put(TransportConstants.KEYSTORE_PATH_PROP_NAME,
-                "/var/data/qiot/machinery/data/runtime/client.ts");
-        map.put(TransportConstants.TRUSTSTORE_PROVIDER_PROP_NAME, "PKCS12");
-        map.put(TransportConstants.TRUSTSTORE_PASSWORD_PROP_NAME, "password");
+        // verifyHost
+        map.put(TransportConstants.VERIFY_HOST_PROP_NAME, verifyHost);
+        // trustAll
+        map.put(TransportConstants.TRUST_ALL_PROP_NAME, trustAll);
+        // keyStoreProvider
+        map.put(TransportConstants.KEYSTORE_PROVIDER_PROP_NAME,
+                keyStoreProvider);
+        // keyStorePath
+        map.put(TransportConstants.KEYSTORE_PATH_PROP_NAME, keyStorePath);
+        // keyStorePassword
+        map.put(TransportConstants.KEYSTORE_PASSWORD_PROP_NAME,
+                keyStorePassword);
+        // trustStoreProvider
+        map.put(TransportConstants.TRUSTSTORE_PROVIDER_PROP_NAME,
+                trustStoreProvider);
+        // trustStorePath
+        map.put(TransportConstants.TRUSTSTORE_PATH_PROP_NAME, trustStorePath);
+        // trustStorePassword
+        map.put(TransportConstants.TRUSTSTORE_PASSWORD_PROP_NAME,
+                trustStorePassword);
 
         TransportConfiguration tc = new TransportConfiguration(
                 NettyConnectorFactory.class.getName(), map);
